80021fbdf6cc99d3d8e8c736c59fbeb048bd07bf
Spote777 <66115092+Spote777@users.noreply.github.com>
2022-08-23 13:47:29 +0300

    Initial Commit

---
+++ GBSlotMachineSwiftUI/GBSlotMachineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBSlotMachineSwiftUIApp.swift
+//  GBSlotMachineSwiftUI
+//
+//  Created by Павел Заруцков on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GBSlotMachineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
